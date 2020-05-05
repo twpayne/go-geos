458d73c231c0adeaf4c13ec8620d4816c248f107
Tom Payne <twpayne@gmail.com>
2020-05-05 19:26:09 +0100

    Run clang-format

--- geos.c
+++ geos.c
@@ -9,8 +9,10 @@ enum { bounds_MinX, bounds_MinY, bounds_MaxX, bounds_MaxY };
 // c_GEOSCoordSeq_getFlatCoords_r writes s's coordinate data to flatCoords,
 // which must contain size*dims elements. It returns 0 on any exception, 1
 // otherwise.
-int c_GEOSCoordSeq_getFlatCoords_r(GEOSContextHandle_t handle, const GEOSCoordSequence *s, unsigned int size,
-                                   unsigned int dims, double *flatCoords) {
+int c_GEOSCoordSeq_getFlatCoords_r(GEOSContextHandle_t handle,
+                                   const GEOSCoordSequence *s,
+                                   unsigned int size, unsigned int dims,
+                                   double *flatCoords) {
   double *val = flatCoords;
   for (unsigned int idx = 0; idx < size; ++idx) {
     for (unsigned int dim = 0; dim < dims; ++dim) {
@@ -23,16 +25,41 @@ int c_GEOSCoordSeq_getFlatCoords_r(GEOSContextHandle_t handle, const GEOSCoordSe
 }
 
 // c_GEOSGeomBounds_r extends bounds to include g.
-void c_GEOSGeomBounds_r(GEOSContextHandle_t handle, const GEOSGeometry *g, double *minX, double *minY, double *maxX,
+void c_GEOSGeomBounds_r(GEOSContextHandle_t handle, const GEOSGeometry *g,
+                        double *minX, double *minY, double *maxX,
                         double *maxY) {
   if (GEOSisEmpty_r(handle, g)) {
     return;
   }
 
   switch (GEOSGeomTypeId_r(handle, g)) {
-    case GEOS_POINT: {
+  case GEOS_POINT: {
+    double x;
+    GEOSGeomGetX_r(handle, g, &x);
+    if (x < *minX) {
+      *minX = x;
+    }
+    if (x > *maxX) {
+      *maxX = x;
+    }
+    double y;
+    GEOSGeomGetY_r(handle, g, &y);
+    if (y < *minY) {
+      *minY = y;
+    }
+    if (y > *maxY) {
+      *maxY = y;
+    }
+  } break;
+  case GEOS_LINESTRING:
+    // fallthrough
+  case GEOS_LINEARRING: {
+    const GEOSCoordSequence *s = GEOSGeom_getCoordSeq_r(handle, g);
+    unsigned int size;
+    GEOSCoordSeq_getSize_r(handle, s, &size);
+    for (int i = 0; i < size; ++i) {
       double x;
-      GEOSGeomGetX_r(handle, g, &x);
+      GEOSCoordSeq_getX_r(handle, s, i, &x);
       if (x < *minX) {
         *minX = x;
       }
@@ -40,63 +67,43 @@ void c_GEOSGeomBounds_r(GEOSContextHandle_t handle, const GEOSGeometry *g, doubl
         *maxX = x;
       }
       double y;
-      GEOSGeomGetY_r(handle, g, &y);
+      GEOSCoordSeq_getY_r(handle, s, i, &y);
       if (y < *minY) {
         *minY = y;
       }
       if (y > *maxY) {
         *maxY = y;
       }
-    } break;
-    case GEOS_LINESTRING:
-      // fallthrough
-    case GEOS_LINEARRING: {
-      const GEOSCoordSequence *s = GEOSGeom_getCoordSeq_r(handle, g);
-      unsigned int size;
-      GEOSCoordSeq_getSize_r(handle, s, &size);
-      for (int i = 0; i < size; ++i) {
-        double x;
-        GEOSCoordSeq_getX_r(handle, s, i, &x);
-        if (x < *minX) {
-          *minX = x;
-        }
-        if (x > *maxX) {
-          *maxX = x;
-        }
-        double y;
-        GEOSCoordSeq_getY_r(handle, s, i, &y);
-        if (y < *minY) {
-          *minY = y;
-        }
-        if (y > *maxY) {
-          *maxY = y;
-        }
-      }
-    } break;
-    case GEOS_POLYGON:
-      c_GEOSGeomBounds_r(handle, GEOSGetExteriorRing_r(handle, g), minX, minY, maxX, maxY);
-      for (int i = 0, n = GEOSGetNumInteriorRings_r(handle, g); i < n; ++i) {
-        c_GEOSGeomBounds_r(handle, GEOSGetInteriorRingN_r(handle, g, i), minX, minY, maxX, maxY);
-      }
-      break;
-    case GEOS_MULTIPOINT:
-      // fallthrough
-    case GEOS_MULTILINESTRING:
-      // fallthrough
-    case GEOS_MULTIPOLYGON:
-      // fallthrough
-    case GEOS_GEOMETRYCOLLECTION:
-      for (int i = 0, n = GEOSGetNumGeometries_r(handle, g); i < n; ++i) {
-        c_GEOSGeomBounds_r(handle, GEOSGetGeometryN_r(handle, g, i), minX, minY, maxX, maxY);
-      }
-      break;
+    }
+  } break;
+  case GEOS_POLYGON:
+    c_GEOSGeomBounds_r(handle, GEOSGetExteriorRing_r(handle, g), minX, minY,
+                       maxX, maxY);
+    for (int i = 0, n = GEOSGetNumInteriorRings_r(handle, g); i < n; ++i) {
+      c_GEOSGeomBounds_r(handle, GEOSGetInteriorRingN_r(handle, g, i), minX,
+                         minY, maxX, maxY);
+    }
+    break;
+  case GEOS_MULTIPOINT:
+    // fallthrough
+  case GEOS_MULTILINESTRING:
+    // fallthrough
+  case GEOS_MULTIPOLYGON:
+    // fallthrough
+  case GEOS_GEOMETRYCOLLECTION:
+    for (int i = 0, n = GEOSGetNumGeometries_r(handle, g); i < n; ++i) {
+      c_GEOSGeomBounds_r(handle, GEOSGetGeometryN_r(handle, g, i), minX, minY,
+                         maxX, maxY);
+    }
+    break;
   }
 }
 
 // c_GEOSGeomGetInfo_r returns information about g. It returns 0 on any
 // exception, 1 otherwise.
-int c_GEOSGeomGetInfo_r(GEOSContextHandle_t handle, const GEOSGeometry *g, int *typeID, int *numGeometries,
-                        int *numPoints, int *numInteriorRings) {
+int c_GEOSGeomGetInfo_r(GEOSContextHandle_t handle, const GEOSGeometry *g,
+                        int *typeID, int *numGeometries, int *numPoints,
+                        int *numInteriorRings) {
   *typeID = GEOSGeomTypeId_r(handle, g);
   if (*typeID == -1) {
     return 0;
@@ -106,20 +113,20 @@ int c_GEOSGeomGetInfo_r(GEOSContextHandle_t handle, const GEOSGeometry *g, int *
     return 0;
   }
   switch (*typeID) {
-    case GEOS_LINESTRING:
-      // fallthrough
-    case GEOS_LINEARRING:
-      *numPoints = GEOSGeomGetNumPoints_r(handle, g);
-      if (*numPoints == -1) {
-        return 0;
-      }
-      break;
-    case GEOS_POLYGON:
-      *numInteriorRings = GEOSGetNumInteriorRings_r(handle, g);
-      if (*numInteriorRings == -1) {
-        return 0;
-      }
-      break;
+  case GEOS_LINESTRING:
+    // fallthrough
+  case GEOS_LINEARRING:
+    *numPoints = GEOSGeomGetNumPoints_r(handle, g);
+    if (*numPoints == -1) {
+      return 0;
+    }
+    break;
+  case GEOS_POLYGON:
+    *numInteriorRings = GEOSGetNumInteriorRings_r(handle, g);
+    if (*numInteriorRings == -1) {
+      return 0;
+    }
+    break;
   }
   return 1;
 }
@@ -131,7 +138,9 @@ void c_errorMessageHandler(const char *message, void *userdata) {
 
 // c_newGEOSCoordSeqFromFlatCoords returns a new GEOSCoordSequence populated
 // with flatCoords. It returns NULL on any exception.
-GEOSCoordSequence *c_newGEOSCoordSeqFromFlatCoords_r(GEOSContextHandle_t handle, unsigned int size, unsigned int dims,
+GEOSCoordSequence *c_newGEOSCoordSeqFromFlatCoords_r(GEOSContextHandle_t handle,
+                                                     unsigned int size,
+                                                     unsigned int dims,
                                                      const double *flatCoords) {
   GEOSCoordSequence *s = GEOSCoordSeq_create_r(handle, size, dims);
   if (s == NULL) {
@@ -151,7 +160,8 @@ GEOSCoordSequence *c_newGEOSCoordSeqFromFlatCoords_r(GEOSContextHandle_t handle,
 
 // c_newGEOSGeomFromBounds_r returns a new GEOSGeom representing bounds. It
 // returns NULL on any exception.
-GEOSGeometry *c_newGEOSGeomFromBounds_r(GEOSContextHandle_t handle, int *typeID, double minX, double minY, double maxX,
+GEOSGeometry *c_newGEOSGeomFromBounds_r(GEOSContextHandle_t handle, int *typeID,
+                                        double minX, double minY, double maxX,
                                         double maxY) {
   if (minX > maxX || minY > maxY) {
     *typeID = GEOS_POINT;
@@ -162,7 +172,8 @@ GEOSGeometry *c_newGEOSGeomFromBounds_r(GEOSContextHandle_t handle, int *typeID,
     if (s == NULL) {
       return NULL;
     }
-    if (GEOSCoordSeq_setX_r(handle, s, 0, minX) == 0 || GEOSCoordSeq_setY_r(handle, s, 0, minY) == 0) {
+    if (GEOSCoordSeq_setX_r(handle, s, 0, minX) == 0 ||
+        GEOSCoordSeq_setY_r(handle, s, 0, minY) == 0) {
       GEOSCoordSeq_destroy_r(handle, s);
       return NULL;
     }
@@ -178,10 +189,12 @@ GEOSGeometry *c_newGEOSGeomFromBounds_r(GEOSContextHandle_t handle, int *typeID,
   if (s == NULL) {
     return NULL;
   }
-  const double flatCoords[10] = {minX, minY, maxX, minY, maxX, maxY, minX, maxY, minX, minY};
-  const double *val           = flatCoords;
+  const double flatCoords[10] = {minX, minY, maxX, minY, maxX,
+                                 maxY, minX, maxY, minX, minY};
+  const double *val = flatCoords;
   for (unsigned idx = 0; idx < 5; idx++) {
-    if (GEOSCoordSeq_setX_r(handle, s, idx, *val++) == 0 || GEOSCoordSeq_setY_r(handle, s, idx, *val++) == 0) {
+    if (GEOSCoordSeq_setX_r(handle, s, idx, *val++) == 0 ||
+        GEOSCoordSeq_setY_r(handle, s, idx, *val++) == 0) {
       GEOSCoordSeq_destroy_r(handle, s);
       return NULL;
     }
