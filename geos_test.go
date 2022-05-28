3cb51213d3d3ee4e0bb331886cb52f978b6cf4d1
Tom Payne <twpayne@gmail.com>
2022-05-28 22:07:20 +0200

    Merge test utility files

--- geos_test.go
+++ geos_test.go
@@ -1,6 +1,18 @@
 package geos
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func mustNewGeomFromWKT(t *testing.T, c *Context, wkt string) *Geom {
+	t.Helper()
+	geom, err := c.NewGeomFromWKT(wkt)
+	require.NoError(t, err)
+	require.True(t, geom.IsValid())
+	return geom
+}
 
 func skipIfVersionLessThan(t *testing.T, major, minor, patch int) {
 	t.Helper()
