50044ff571f1c343f23ec810394324ab44ff7532
Tom Payne <twpayne@gmail.com>
2024-07-06 02:10:00 +0200

    Add GeoJSON FeatureCollection test

--- geojson/geojson_test.go
+++ geojson/geojson_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/twpayne/go-geos/geometry"
 )
 
-func TestGeoJSON(t *testing.T) {
+func TestFeature(t *testing.T) {
 	for i, tc := range []struct {
 		feat       *geojson.Feature
 		geoJSONStr string
@@ -35,6 +35,59 @@ func TestGeoJSON(t *testing.T) {
 			var feat geojson.Feature
 			assert.NoError(t, feat.UnmarshalJSON([]byte(tc.geoJSONStr)))
 			assert.True(t, tc.feat.Geometry.Equals(feat.Geometry.Geom))
+		})
+	}
+}
+
+func TestFeatureCollection(t *testing.T) {
+	for i, tc := range []struct {
+		featureCollection geojson.FeatureCollection
+		geoJSONStr        string
+	}{
+		{
+			featureCollection: geojson.FeatureCollection{},
+			geoJSONStr:        `{"type":"FeatureCollection","features":[]}`,
+		},
+		{
+			featureCollection: geojson.FeatureCollection{
+				{
+					ID:       "point",
+					Geometry: *geometry.NewGeometry(geos.NewPoint([]float64{1, 2})),
+					Properties: map[string]interface{}{
+						"key": "value",
+					},
+				},
+			},
+			geoJSONStr: `{"type":"FeatureCollection","features":[{"id":"point","type":"Feature","geometry":{"type":"Point","coordinates":[1,2]},"properties":{"key":"value"}}]}`,
+		},
+		{
+			featureCollection: geojson.FeatureCollection{
+				{
+					ID:       "point",
+					Geometry: *geometry.NewGeometry(geos.NewPoint([]float64{1, 2})),
+					Properties: map[string]interface{}{
+						"key": "value",
+					},
+				},
+				{
+					ID:       "linestring",
+					Geometry: *geometry.NewGeometry(geos.NewLineString([][]float64{{1, 2}, {3, 4}})),
+					Properties: map[string]interface{}{
+						"key": "value",
+					},
+				},
+			},
+			geoJSONStr: `{"type":"FeatureCollection","features":[{"id":"point","type":"Feature","geometry":{"type":"Point","coordinates":[1,2]},"properties":{"key":"value"}},{"id":"linestring","type":"Feature","geometry":{"type":"LineString","coordinates":[[1,2],[3,4]]},"properties":{"key":"value"}}]}`,
+		},
+	} {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			actualGeoJSON, err := tc.featureCollection.MarshalJSON()
+			assert.NoError(t, err)
+			assert.Equal(t, tc.geoJSONStr, string(actualGeoJSON))
+
+			var featureCollection geojson.FeatureCollection
+			assert.NoError(t, featureCollection.UnmarshalJSON([]byte(tc.geoJSONStr)))
+			assert.Equal(t, tc.featureCollection, featureCollection)
 		})
 	}
 }
