b9ad841c43e1b1c18850469516b7075292ad75f6
Tom Payne <twpayne@gmail.com>
2025-07-16 17:24:42 +0000

    feat: add some examples

---
+++ example_test.go
@@ -0,0 +1,29 @@
+package geos_test
+
+import (
+	"fmt"
+
+	"github.com/twpayne/go-geos"
+)
+
+func ExampleNewGeomFromWKT() {
+	geom, err := geos.NewGeomFromWKT("POINT(1 1)")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(geom.ToWKT())
+	// Output: POINT (1 1)
+}
+
+func ExamplePrepGeom() {
+	geom, err := geos.NewGeomFromWKT("POLYGON ((189 115, 200 170, 130 170, 35 242, 156 215, 210 290, 274 256, 360 190, 267 215, 300 50, 200 60, 189 115))")
+	if err != nil {
+		panic(err)
+	}
+	prepGeom := geom.Prepare()
+	point := geos.NewPointFromXY(190, 200)
+	if prepGeom.Intersects(point) {
+		fmt.Println("intersects")
+	}
+	// Output: intersects
+}
