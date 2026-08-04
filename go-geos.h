d315b418ba78366e1426fc58e7f475034f20f2b0
Tom Payne <twpayne@gmail.com>
2026-08-04 16:29:50 +0000

    chore: re-enable GEOS_USE_ONLY_R_API flag

--- go-geos.h
+++ go-geos.h
@@ -3,7 +3,21 @@
 
 #include <stdint.h>
 
+#define GEOS_USE_ONLY_R_API
 #include <geos_c.h>
+
+#if GEOS_VERSION_MAJOR < 3 ||                                                  \
+    (GEOS_VERSION_MAJOR == 3 && GEOS_VERSION_MINOR < 15)
+enum GEOSVoronoiFlags {
+  /** Return only edges of the Voronoi cells, as a MultiLineString **/
+  GEOS_VORONOI_ONLY_EDGES = 1,
+  /** Preserve order of inputs, such that the nth cell in the result corresponds
+   *  to the nth vertex in the input. If this cannot be done, such as for inputs
+   *  that contain repeated points, \ref GEOSVoronoiDiagram will return NULL.
+   * **/
+  GEOS_VORONOI_PRESERVE_ORDER = 2
+};
+#endif
 
 uintptr_t c_GEOSGeom_getUserData_r(GEOSContextHandle_t handle,
                                    const GEOSGeometry *g);
